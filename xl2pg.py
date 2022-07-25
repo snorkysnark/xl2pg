5c166539c43de639a1393ef50236f39aa701fab5
snorkysnark <snorkysnark@gmail.com>
2022-07-25 13:04:22 +0300

    rewrite using psycopg3

--- xl2pg.py
+++ xl2pg.py
@@ -1,95 +1,102 @@
 from argparse import ArgumentParser
 import json
+from pathlib import Path
+from typing import TypedDict
+import textwrap
+import sys
+
 import openpyxl
-from openpyxl.worksheet._write_only import WriteOnlyWorksheet
-import psycopg2
-from lib import dbconfig
-##
+import psycopg
+from psycopg.sql import SQL, Identifier, Placeholder
 
-def parse_args():
-    argp = ArgumentParser()
-    argp.add_argument('spreadsheet', help='xlsx file to upload')
-    argp.add_argument('-d', '--db', help='Database config as a json file: { "dbname": "", "user": "", "password": "" }')
-    argp.add_argument('-m', '--map', required=True, help='Json mapping from excel fields to postgres table')
-    argp.add_argument('--clear', action='store_true', help='perform DELETE FROM target table')
-    return argp.parse_args()
-args = parse_args()
-##
 
-db = dbconfig.load_or_prompt(args.db)
-##
+class MappingConfig(TypedDict):
+    target_schema: str
+    target_table: str
+    sheet_number: int
+    skip_rows: int
+    mappings: dict[str, int]
 
-def load_map(path):
-    with open(path) as file:
-        return json.load(file)
-map_cfg = load_map(args.map)
 
-target_table = map_cfg['target_table']
-sheet_number = map_cfg['sheet']
-skip_rows = map_cfg['skip_rows']
-mappings = map_cfg['mapping']
-##
+def printerr(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
 
-def generate_prepare_statement(table_name, mappings):
-    field_names = []
-    variables = []
 
-    for index, field in enumerate(mappings.keys()):
-        field_names.append(field)
-        variables.append('$' + str(index + 1))
-
-    return '''
-PREPARE xl2pg_insert AS
-INSERT INTO {table}(
-    {fields}
-) VALUES (
-    {values}
-);
-    '''.format(
-        table=table_name,
-        fields=',\n    '.join(field_names),
-        values=',\n    '.join(variables)
+def upload(
+    conn: psycopg.Connection,
+    wb: openpyxl.Workbook,
+    map_cfg: MappingConfig,
+    clear: bool = False,
+):
+    table_path = SQL("{}.{}").format(
+        Identifier(map_cfg["target_schema"], map_cfg["target_table"])
     )
-##
+    insert_statement = SQL(
+        textwrap.dedent(
+            """\
+            INSERT INTO {table}(
+                {fields}
+            ) VALUES ({values})"""
+        )
+    ).format(
+        table=table_path,
+        fields=SQL(",\n").join(map(Identifier, map_cfg["mappings"].keys())),
+        values=SQL(", ").join([Placeholder()] * len(map_cfg["mappings"])),
+    )
+    printerr("Generated statement:")
+    printerr(insert_statement.as_string(conn))
+    printerr()
 
-prep_statement = generate_prepare_statement(target_table, mappings)
-print('Generated statement:')
-print(prep_statement)
-print()
-##
+    sheet = wb.worksheets[map_cfg["sheet_number"]]
 
-def generate_execute_statement(mappings):
-    return 'EXECUTE xl2pg_insert(' + ', '.join(['%s'] * len(mappings)) + ')'
-exec_statement = generate_execute_statement(mappings)
-##
-
-print('Connecting to', db.dbname)
-with psycopg2.connect(dbname=db.dbname, user=db.user, password=db.password, host=db.host, port=db.port) as conn:
     with conn.cursor() as cursor:
-        print('Preparing insert statement')
-        cursor.execute(prep_statement)
-
-        print('Loading spreadsheet')
-        wb = openpyxl.load_workbook(args.spreadsheet)
-        sheet = wb.worksheets[sheet_number]
-        assert not isinstance(sheet, WriteOnlyWorksheet), 'Worksheet is write only'
-
-        if args.clear:
-            print('Clearing', target_table)
-            cursor.execute(f'DELETE FROM {target_table};')
-
+        if clear:
+            printerr(f"Clearing {table_path.as_string(cursor)}")
+            cursor.execute(SQL("DELETE FROM {}").format(table_path))
 
         max_row = sheet.max_row
-        assert max_row, 'Cannot get max_row of spreadsheet'
-
-        for row_index in range(skip_rows + 1, max_row + 1):
+        for row_index in range(map_cfg["skip_rows"] + 1, max_row + 1):
             output_row = []
 
-            for column_index in mappings.values():
+            for column_index in map_cfg.values():
                 value = sheet.cell(row_index, column_index).value
                 output_row.append(value)
-            print('Inserting row', row_index, 'out of', max_row, end='\r', flush=True)
-            cursor.execute(exec_statement, output_row)
-        print()
+            printerr(
+                "Inserting row", row_index, "out of", max_row, end="\r", flush=True
+            )
 
-    conn.commit()
+            cursor.execute(insert_statement, output_row, prepare=True)
+        printerr()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("spreadsheet", type=Path, help="xlsx file to upload")
+    parser.add_argument(
+        "-d",
+        "--db",
+        type=Path,
+        required=True,
+        help='Database config as a json file: { "dbname": "", "user": "", "password": "" }',
+    )
+    parser.add_argument(
+        "-m",
+        "--map",
+        required=True,
+        type=Path,
+        help="Json mapping from excel fields to postgres table",
+    )
+    parser.add_argument(
+        "--clear", action="store_true", help="perform DELETE FROM target table"
+    )
+    args = parser.parse_args()
+
+    conninfo = json.loads(args.db.read_text())
+    map_cfg: MappingConfig = json.loads(args.map.read_text())
+
+    printerr("Loading spreadsheet")
+    wb = openpyxl.load_workbook(args.spreadsheet)
+
+    printerr("Connecting to database")
+    with psycopg.connect(**conninfo) as conn:
+        upload(conn=conn, wb=wb, map_cfg=map_cfg, clear=args.clear)
